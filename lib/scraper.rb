f8ff41a932a2cd2f9c521f34321798213d37fee7
Kirsten Werner <kirstenmwerner@gmail.com>
2019-03-04 22:55:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,16 @@ require_relative './course.rb'
 
 class Scraper
   
-    def print_courses
+  def get_page
+  end 
+  
+  def get_courses
+  end 
+  
+  def make_courses
+  end 
+  
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
